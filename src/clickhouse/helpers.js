bc78ab8746363a6b54312612b543c43f668dd26e
cdROma <38502733+gthrm@users.noreply.github.com>
2025-01-16 18:05:45 +0100

    fix: ClickHouse disconected

--- src/clickhouse/helpers.js
+++ src/clickhouse/helpers.js
@@ -1,10 +1,10 @@
 const logger = require('../utils/logger');
-const { Message } = require('./models/Message');
+// const { Message } = require('./models/Message');
 
 async function logMessageEvent(logMessage, customEventString) {
   try {
-    logger.info('New Message', logMessage);
-    await Message.logMessageEvent({ ...logMessage, custom: customEventString });
+    logger.info('New Message', { logMessage, customEventString });
+    // await Message.logMessageEvent({ ...logMessage, custom: customEventString });
   } catch (error) {
     logger.error('Error handling message:', error, logMessage);
   }
--- src/clickhouse/migrations/index.js
+++ src/clickhouse/migrations/index.js
@@ -1,59 +1,60 @@
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable no-await-in-loop */
-const path = require('path');
-const fs = require('fs/promises');
-const { getClickHouseClient } = require('../../db');
-const logger = require('../../utils/logger');
+// const path = require('path');
+// const fs = require('fs/promises');
+// const { getClickHouseClient } = require('../../db');
+// const logger = require('../../utils/logger');
 
 async function runMigrations() {
-  const client = getClickHouseClient();
+  return Promise.resolve();
+  // const client = getClickHouseClient();
 
-  logger.info('Running migrations...');
+  // logger.info('Running migrations...');
 
-  await client.exec({
-    query: `
-      CREATE TABLE IF NOT EXISTS migrations (
-        id UInt32,
-        name String,
-        executed_at DateTime DEFAULT now()
-      ) ENGINE = MergeTree()
-      ORDER BY id
-      PRIMARY KEY id
-      SETTINGS index_granularity = 8192;
-    `,
-  });
+  // await client.exec({
+  //   query: `
+  //     CREATE TABLE IF NOT EXISTS migrations (
+  //       id UInt32,
+  //       name String,
+  //       executed_at DateTime DEFAULT now()
+  //     ) ENGINE = MergeTree()
+  //     ORDER BY id
+  //     PRIMARY KEY id
+  //     SETTINGS index_granularity = 8192;
+  //   `,
+  // });
 
-  logger.info('Migrations table created');
+  // logger.info('Migrations table created');
 
-  const data = await client.query({
-    query: 'SELECT name FROM migrations',
-    format: 'JSONEachRow',
-  });
+  // const data = await client.query({
+  //   query: 'SELECT name FROM migrations',
+  //   format: 'JSONEachRow',
+  // });
 
-  const executedMigrations = await data.json();
+  // const executedMigrations = await data.json();
 
-  const executedMigrationNames = new Set(executedMigrations.map((m) => m.name));
+  // const executedMigrationNames = new Set(executedMigrations.map((m) => m.name));
 
-  const migrationsDir = path.join(__dirname, 'scripts');
-  const migrationFiles = await fs.readdir(migrationsDir);
+  // const migrationsDir = path.join(__dirname, 'scripts');
+  // const migrationFiles = await fs.readdir(migrationsDir);
 
-  for (const file of migrationFiles) {
-    if (file.endsWith('.sql') && !executedMigrationNames.has(file)) {
-      const filePath = path.join(migrationsDir, file);
-      const migrationSql = await fs.readFile(filePath, 'utf-8');
+  // for (const file of migrationFiles) {
+  //   if (file.endsWith('.sql') && !executedMigrationNames.has(file)) {
+  //     const filePath = path.join(migrationsDir, file);
+  //     const migrationSql = await fs.readFile(filePath, 'utf-8');
 
-      logger.info(`Running migration: ${file}`);
-      await client.exec({ query: migrationSql });
+  //     logger.info(`Running migration: ${file}`);
+  //     await client.exec({ query: migrationSql });
 
-      await client.insert({
-        table: 'migrations',
-        values: [{ id: executedMigrations.length + 1, name: file }],
-        format: 'JSONEachRow',
-      });
+  //     await client.insert({
+  //       table: 'migrations',
+  //       values: [{ id: executedMigrations.length + 1, name: file }],
+  //       format: 'JSONEachRow',
+  //     });
 
-      logger.info(`Migration completed: ${file}`);
-    }
-  }
+  //     logger.info(`Migration completed: ${file}`);
+  //   }
+  // }
 }
 
 module.exports = { runMigrations };
